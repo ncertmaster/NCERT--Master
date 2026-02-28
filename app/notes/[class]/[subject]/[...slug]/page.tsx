71f614d0cdd507e5cf1e9381091c94fa07c7ec61
ncertmaster <farhankhan09772@gmail.com>
2026-02-28 12:12:36 +0530

    Delete app/notes/[class]/[subject]/[...slug] directory

--- app/notes/[class]/[subject]/[...slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-export const dynamic = "force-dynamic"
-
-export default function Page({ params }: any) {
-  return (
-    <div style={{ padding: 20 }}>
-      <pre>{JSON.stringify(params, null, 2)}</pre>
-    </div>
-  )
-}
