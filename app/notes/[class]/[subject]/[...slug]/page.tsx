2c46d84042419558538985f81f69cc6e25729221
ncertmaster <farhankhan09772@gmail.com>
2026-02-28 15:20:55 +0530

    Update page.tsx

--- app/notes/[class]/[subject]/[...slug]/page.tsx
+++ app/notes/[class]/[subject]/[...slug]/page.tsx
@@ -1,1 +1,16 @@
-
+export default function NotesPage({
+  params,
+}: {
+  params: {
+    class: string;
+    subject: string;
+    slug: string[];
+  };
+}) {
+  return (
+    <div>
+      <h1>Class: {params.class}</h1>
+      <h2>Subject: {params.subject}</h2>
+    </div>
+  );
+}
