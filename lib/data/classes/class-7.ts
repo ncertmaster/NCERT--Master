35a0c5e050eba30395bbee81620f2e538107922c
Farhan Khan <farhankhan09772@gmail.com>
2026-03-19 03:12:22 +0530

    Create class-7.ts

---
+++ lib/data/classes/class-7.ts
@@ -0,0 +1,7 @@
+import type { Subject } from "../types"
+
+export const class7Subjects: Subject[] = []
+
+export const subjectsByClass7: Partial<Record<number, Subject[]>> = {
+  7: class7Subjects,
+}
