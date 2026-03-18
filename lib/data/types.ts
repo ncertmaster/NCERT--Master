dad770d552dec9df96bd661408c1ad49ebada446
Farhan Khan <farhankhan09772@gmail.com>
2026-03-19 03:01:00 +0530

    Create types.ts

---
+++ lib/data/types.ts
@@ -0,0 +1,29 @@
+export type ClassNumber = 6 | 7 | 8 | 9 | 10 | 11 | 12
+
+export type Chapter = {
+  id: string
+  name: string
+  nameHi: string
+}
+
+export type Book = {
+  id: string
+  name: string
+  nameHi: string
+  chapters: Chapter[]
+}
+
+export type Subject = {
+  id: string
+  name: string
+  nameHi: string
+  icon: string
+  books: Book[]
+}
+
+export type Stream = {
+  id: string
+  name: string
+  nameHi: string
+  subjects: Subject[]
+}
