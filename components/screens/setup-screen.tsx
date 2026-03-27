6e6d47bfcfe81be2b0d3108c5e4953282105e51b
Farhan Khan <farhankhan09772@gmail.com>
2026-03-28 01:35:15 +0530

    Update setup-screen.tsx

--- components/screens/setup-screen.tsx
+++ components/screens/setup-screen.tsx
@@ -61,7 +61,7 @@ export function SetupScreen() {
             {/* Logo */}
             <div className="flex flex-col items-center mb-8">
               <img
-                src="/images/logo.png"
+                src="/logo.png"
                 alt="NCERT Master"
                 className="h-24 w-24 object-contain mb-4"
               />
@@ -177,4 +177,4 @@ export function SetupScreen() {
   )
                                  }
 
-                                   
+                
