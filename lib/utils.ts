0532d2a46d8929764ca8caf25ca92879e1b0125c
Farhan Khan <farhankhan09772@gmail.com>
2026-04-15 18:20:21 +0530

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export function getUserId(): string {
+  if (typeof window === "undefined") return "server"
+  let uid = localStorage.getItem("ncert_uid_v1")
+  if (!uid) {
+    uid = crypto.randomUUID()
+    localStorage.setItem("ncert_uid_v1", uid)
+  }
+  return uid
+}
